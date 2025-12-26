4a1bdbeaae6058b2c3dc5a3f573c0abd6685802d
Akerke Shynarbek <98280652+akerkeshynarbek@users.noreply.github.com>
2025-12-26 18:12:37 +0500

    Update hybrid_ids_final.py

--- hybrid_ids_final.py
+++ hybrid_ids_final.py
@@ -3,10 +3,10 @@ import pandas as pd
 from scapy.all import sniff, IP, TCP, ICMP
 import warnings
 
-# Ignore those annoying 'feature names' warnings
+# Ignoring  not needed warnings
 warnings.filterwarnings("ignore", category=UserWarning)
 
-# Load the brain
+# Loading the brain
 print("Loading Optimized ML Model Brain...")
 model = joblib.load('optimized_ids_model.pkl')
 
@@ -14,26 +14,26 @@ def hybrid_detection(packet):
     if packet.haslayer(IP):
         ip_src = packet[IP].src
 
-        # --- 1. RULE-BASED LAYER (Fast Check) ---
+        #RULE-BASED LAYER
         if packet.haslayer(ICMP):
             print(f"[!] RULE ALERT: Ping/ICMP detected from {ip_src}")
             return
 
-        # --- 2. MACHINE LEARNING LAYER (Smart Check) ---
+        #MACHINE LEARNING LAYER
         if packet.haslayer(TCP):
-            # Extract features exactly as the model expects
+            # Extracting features as the model expects
             dest_port = packet[TCP].dport
             features = [[dest_port, 100, 1, 1]]
-            # Use probability for higher sensitivity
+            #probability for higher sensitivity
             proba = model.predict_proba(features)[0][1]
 
-            # If the probability is high, it's an attack
+            # if the probability is high, i consider it as an attack
             if proba > 0.5:
                 print(f"[!!!] ML ALERT: Malicious Pattern (Confidence: {proba:.2f}) from {ip_src}")
             else:
-                # This helps you see that it's actually working
+                # This helps me see that it's actually working
                 print(f"[-] Normal TCP traffic from {ip_src} (Safe: {1-proba:.2f})")
 
 print("--- HYBRID IDS: MONITORING ACTIVE ---")
-# Start sniffing - using 'lo' for localhost testing
+# Starting sniffing
 sniff(iface="lo", prn=hybrid_detection, store=0)
